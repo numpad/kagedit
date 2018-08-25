5ea682ddd9752c3fd894f6170ed9e4916e91350b
numpad <christian@tuta.io>
2018-08-25 03:23:11 +0200

    vec2_util grows

--- KingArthursEditor/assets/scripts/modules/vec2_util.lua
+++ KingArthursEditor/assets/scripts/modules/vec2_util.lua
@@ -1,12 +1,34 @@
 
+-- collection of additional vec2 utility functions
+
+---- Documentation ----
+--[[
+usage:
+	local vec2x = require('vec2_util')
+
+	local rand_dir = vec2x.random() -- Returns a random direction vector with length 1.
+]]
+
 local M = {}
 
-M.random = function()
+M.random = function ()
 	local random_angle = math.random() * 2 * math.pi
 	return vec2.new(
 		math.cos(random_angle),
 		math.sin(random_angle)
 	)
-end 
+end
+
+M.clamp = function (vec, min_len, max_len)
+	local len = vec2.length(vec)
+	
+	if len < min_len then
+		len = min_len
+	elseif len > max_len then
+		len = max_len
+	end
+	
+	return vec:normalize() * len
+end
 
 return M
